f52185e4a3d577edb102212e221f42e85d8b5840
Sowren Sen <sowrensen@gmail.com>
2024-11-02 22:49:42 +0600

    bulk import expenses

---
+++ app/Filament/Imports/ExpenseImporter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Filament\Imports;
+
+use App\Models\Expense;
+use Filament\Actions\Imports\ImportColumn;
+use Filament\Actions\Imports\Importer;
+use Filament\Actions\Imports\Models\Import;
+
+class ExpenseImporter extends Importer
+{
+    protected static ?string $model = Expense::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ImportColumn::make('category')
+                ->relationship(resolveUsing: 'name')
+                ->rules(['required']),
+            ImportColumn::make('price')
+                ->requiredMapping()
+                ->numeric()
+                ->rules(['required']),
+            ImportColumn::make('usable')
+                ->requiredMapping()
+                ->numeric()
+                ->rules(['required']),
+            ImportColumn::make('leftover')
+                ->numeric(),
+            ImportColumn::make('unit'),
+            ImportColumn::make('purchase_date')
+                ->rules(['date']),
+            ImportColumn::make('usage_date')
+                ->rules(['date']),
+        ];
+    }
+
+    public function resolveRecord(): ?Expense
+    {
+        // return Expense::firstOrNew([
+        //     // Update existing records, matching them by `$this->data['column_name']`
+        //     'email' => $this->data['email'],
+        // ]);
+
+        return new Expense;
+    }
+
+    public static function getCompletedNotificationBody(Import $import): string
+    {
+        $body = 'Your expense import has completed and '.number_format($import->successful_rows).' '.str('row')->plural($import->successful_rows).' imported.';
+
+        if ($failedRowsCount = $import->getFailedRowsCount()) {
+            $body .= ' '.number_format($failedRowsCount).' '.str('row')->plural($failedRowsCount).' failed to import.';
+        }
+
+        return $body;
+    }
+}
--- app/Filament/Resources/ExpenseResource/Pages/ListExpenses.php
+++ app/Filament/Resources/ExpenseResource/Pages/ListExpenses.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\ExpenseResource\Pages;
 
+use App\Filament\Imports\ExpenseImporter;
 use App\Filament\Resources\ExpenseResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Colors\Color;
 
 class ListExpenses extends ListRecords
 {
@@ -13,6 +15,10 @@ class ListExpenses extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\ImportAction::make()
+                ->label('Import from file')
+                ->importer(ExpenseImporter::class)
+                ->color(Color::Amber),
             Actions\CreateAction::make(),
         ];
     }
